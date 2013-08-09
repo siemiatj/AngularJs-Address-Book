e20173794589523621660daab209577bf3301162
Łukasz Kliś <lukasz@klis.pl>
2013-08-09 22:21:10 +0200

    Small refactoring of controllers and contactsService

--- application/controllers/controllers.js
+++ application/controllers/controllers.js
@@ -1,84 +1,40 @@
-app.controller('ContactsController', function ($scope, $location, contactsService) {
-
-    (function() {
+app.controller('ContactsController', [
+    '$scope', '$location', 'contactsService',
+    function ($scope, $location, contactsService) {
         $scope.contacts = contactsService.getContacts();
-    })();
 
-    $scope.editContact = function(id) {
-        $location.path('/edit-contact/'+id);
-    };
+        $scope.editContact = function (id) {
+            $location.path('/edit-contact/' + id);
+        };
 
-    $scope.showDetails = function($event, id) {
-        var el = document.getElementById('ct-details-'+id),
-            cName = el.classList;
-
-        if (cName.contains('details-hidden')) {
-            cName.remove('details-hidden');
-        } else {
-            cName.add('details-hidden');
+        $scope.showDetails = function (id) {
+            var el = angular.element(document.getElementById('#ct-details-' + id));
+            el.toggleClass('details-hidden');
         }
     }
-});
+]);
 
-app.controller('ContactAddController', function ($scope, $location, contactsService) {
-    $scope.insertContact = function () {
-        var nC = $scope.newContact;
-
-        var fN = nC.firstName;
-        var lN = nC.lastName;
-        var eA = nC.emailAddress;
-        var aD = nC.address;
-        var zC = nC.zipCode;
-        var cT = nC.city;
-        var cN = nC.country;
-
-        contactsService.addContact(fN, lN, eA, aD, zC, cT, cN);
-        nC.firstName = '';
-        nC.lastName = '';
-        nC.emailAddress = '';
-        nC.address = '';
-        nC.zipCode = '';
-        nC.city = '';
-        nC.country = '';
-
-        $location.path('/contacts');
-    };
-});
-
-app.controller('ContactEditController', function ($scope, $routeParams, $location, contactsService) {
-
-    init();
-
-    $scope.contact = {};
-
-    init();
-
-    function init() {       
-        var contactID = ($routeParams.contactID) ? parseInt($routeParams.contactID) : 0;
-            
-        $scope.contact = contactsService.getContact(contactID, true);
+app.controller('ContactAddController', [
+    '$scope', '$location', 'contactsService',
+    function ($scope, $location, contactsService) {
+        $scope.insertContact = function () {
+            contactsService.addContact($scope.contact);
+            $scope.contact = contactsService.resetContact();
+            $location.path('/contacts');
+        };
     }
+]);
 
-    $scope.editContact = function() {
-        var c = $scope.contact;
+app.controller('ContactEditController', [
+    '$scope', '$routeParams', '$location', 'contactsService',
+    function ($scope, $routeParams, $location, contactsService) {
+        var contactId = $routeParams.contactId ? parseInt($routeParams.contactId, 10) : 0;
+        $scope.contact = contactsService.getContact(contactId, true);
 
-        var fN = c.firstName;
-        var lN = c.lastName;
-        var eA = c.emailAddress;
-        var aD = c.address;
-        var zC = c.zipCode;
-        var cT = c.city;
-        var cN = c.country;
-
-        contactsService.editContact($scope.contact.id, fN, lN, eA, aD, zC, cT, cN);
-        c.firstName = '';
-        c.lastName = '';
-        c.emailAddress = '';
-        c.address = '';
-        c.zipCode = '';
-        c.city = '';
-        c.country = '';
-
-        $location.path('/contacts');
-    };
-});
+        $scope.editContact = function() {
+            contactsService.editContact($scope.contact);
+            $scope.contact = contactsService.resetContact();
+            $location.path('/contacts');
+        };
+    }
+]);
--- application/services/contactsService.js
+++ application/services/contactsService.js
@@ -1,68 +1,62 @@
 
 app.service('contactsService', function () {
-    this.getContacts = function() {
+    this.getContacts = function () {
         return contacts;
     };
 
-    this.addContact = function(fN, lN, eA, aD, zC, cT, cN) {
-        var nextId = contacts.length + 1;
-
-        contacts.push({
-            id: nextId,
-            firstName: fN,
-            lastName: lN,
-            emailAddress: eA,
-            address: aD,
-            city: cT,
-            country: cN
-        });
-    };
-
-    this.editContact = function(id, fN, lN, eA, aD, zC, cT, cN) {
-        var idx = this.getContact(id),
-            contact = contacts[idx];
-
-        contacts[idx] = {
-            id: contact.id,
-            firstName: fN,
-            lastName: lN,
-            emailAddress: eA,
-            address: aD,
-            city: cT,
-            country: cN                    
+    this.resetContact = function () {
+        return {
+            firstName: '',
+            lastName: '',
+            emailAddress: '',
+            address: '',
+            zipCode: '',
+            city: '',
+            country: ''
         };
+    }
+
+    this.addContact = function (contact) {
+        var nextId = contacts.length + 1;
+        contact.id = nextId;
+
+        contacts.push(contact);
     };
 
-    this.getContact = function(id, getObj) {
-        for (var i=0, l=contacts.length; i<l; i+=1) {
+    this.editContact = function (contact) {
+        contacts[contact.id] = contact;
+    };
+
+    this.getContact = function (id, getObj) {
+        for (var i = 0, l = contacts.length; i < l; i += 1) {
             if (contacts[i].id === id) {
                 return getObj ? contacts[i] : i;
 
                 break;
             }
-        }        
+        }
     };
 
     var contacts = [
         {
-            id: 0, 
-            firstName: 'John', 
-            lastName: 'Smith', 
-            address: 'Baywatch Dr. 547.', 
+            id: 0,
+            firstName: 'John',
+            lastName: 'Smith',
+            address: 'Baywatch Dr. 547.',
             city: 'Orlando',
             country: 'USA',
             zipCode: '05100',
             emailAddress: 'john43@yahoo.com'
         },
         {
-            id: 1, 
-            firstName: 'Mike', 
-            lastName: 'Smith', 
-            address: '459 5th Av.', 
+            id: 1,
+            firstName: 'Mike',
+            lastName: 'Smith',
+            address: '459 5th Av.',
             city: 'New York',
             country: 'USA',
             zipCode: '03335',
-            emailAddress: 'mikes5@gmail.com'           
+            emailAddress: 'mikes5@gmail.com'
         }
     ];
 
